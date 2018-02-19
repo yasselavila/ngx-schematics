a89fb8c1c0d19611e3715a8acda62e2461d7e0cf
Yassel Avila <yassel.avila@gmail.com>
2018-02-18 19:24:10 -0500

    Fix Copyright info

--- src/utils/ng-cli/data.ts
+++ src/utils/ng-cli/data.ts
@@ -1,7 +1,7 @@
 /**
  * @yag/ngx-schematics
  *
- * @copyright Copyright (c) 2016 - 2018, Yassel Avila Gil (http://yasselavila.com)
+ * @copyright Copyright (c) 2018, Yassel Avila Gil (http://yasselavila.com)
  * @license   BSD 3 Clause (see LICENSE.txt)
  * @link      https://github.com/yasselavila/ngx-schematics
  */
